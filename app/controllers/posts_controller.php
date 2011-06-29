76597ccd822a2fd292f819f3bfe26bb731b7e4fc
pugiemonn <pugiemonn@gmail.com>
2011-06-30 08:27:37 +0900

    記事本文のビュー追加

--- app/controllers/posts_controller.php
+++ app/controllers/posts_controller.php
@@ -1,7 +1,8 @@
 <?php
 class PostsController extends AppController {
     var $name = 'Posts';
-	function index() {
+	function index()
+    {
         $this->set('posts', $this->Post->find('all'));
 	}
 	
@@ -10,6 +11,18 @@ class PostsController extends AppController {
 		$this->Post->id=$id;
 		$this->set('post',$this->Post->read());
 	}
+
+    function add()
+    {
+        if(!empty($this->data))
+        {
+            if($this->Post->save($this->data))
+            {
+                $this->flash('投稿は保存されました', '/');
+            }
+        }
+        
+    }
 
 }
 ?>
